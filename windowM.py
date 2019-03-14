44b2f794634c8b60ee48aa043630d9ec6cb252f6
Giieniox256 <48292340+Giieniox256@users.noreply.github.com>
2019-03-14 15:34:30 +0100

    instert example

--- windowM.py
+++ windowM.py
@@ -1,4 +1,16 @@
 import tkinter
 
-win = tkinter.Tk()
-win.mainloop()
+def window():
+    win = tkinter.Tk()
+    win.title('Dowloader image')
+    win.geometry('400x200')
+    tkinter.Label(win,text="Podaj adres URL:",).grid(row = 0)
+    #tkinter.Label(win,text="Test pliku1").grid(row = 1)
+    e1 = tkinter.Entry(win)
+    e1.grid(row = 0,column = 2)
+
+
+    win.mainloop()
+
+
+window()
